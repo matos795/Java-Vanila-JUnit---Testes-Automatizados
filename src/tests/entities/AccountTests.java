b16989faa2c81ed1c8938846c7a5260bff65f3b8
Alexandre Matos <matosalexandre795@gmail.com>
2025-12-27 20:28:47 -0300

    Primeiro Teste: Metodo deposit()

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -1,11 +1,21 @@
 package tests.entities;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import entities.Account;
 
 public class AccountTests {
 
     @Test
     public void depositShouldIncreaseBalanceWhenPositiveAmount(){
 
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account ac = new Account(1L, 0.0);
+
+        ac.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, ac.getBalance());
     }
 }
